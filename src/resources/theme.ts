307566f0393a872ba25ad65e6e951d13bc92b40e
Rohan Gupta <guptarohan369@gmail.com>
2023-04-19 10:09:43 +0530

    Add mixins and theme constants

---
+++ src/resources/theme.ts
@@ -0,0 +1,30 @@
+export const ACCENT_COLORS = {
+    COLOR1: '#E0E0E0',
+};
+
+export const THEME_COLORS = {
+    COLOR1: '#AAFF55',
+    COLOR2: '#EFF8E5',
+    COLOR3: '#02D8E9',
+    COLOR4: '#E7F9FA',
+    COLOR5: '#EEEE66',
+    COLOR6: '#F9F9EA',
+    COLOR7: '#FB5072',
+    COLOR8: '#F8E2E6',
+    COLOR9: '#FF5F9F',
+    COLOR10: '#FFD2E4',
+    COLOR11: '#FF973C',
+    COLOR12: '#FDE3CC',
+    COLOR13: '#60F2CB',
+    COLOR14: '#E9F9F5',
+    COLOR15: '#D898FF',
+    COLOR16: '#F2E6F9',
+    COLOR17: '#D3F559',
+    COLOR18: '#F5FCDC',
+    COLOR19: '#FF7850',
+    COLOR20: '#FBD3C8',
+    COLOR21: '#ED5AF8',
+    COLOR22: '#FACBFE',
+    COLOR23: '#90A7F3',
+    COLOR24: '#E2E8FD',
+};
